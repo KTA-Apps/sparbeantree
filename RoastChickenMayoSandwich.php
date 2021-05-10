0c30fdf6b0cb50c7eeae71f74ffe981402bc41cb
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-05-10 10:40:35 +0200

    Update RoastChickenMayoSandwich.php

--- RoastChickenMayoSandwich.php
+++ RoastChickenMayoSandwich.php
@@ -191,7 +191,7 @@ href="../../wp-includes/wlwmanifest.xml" /><meta name="generator" content="WordP
 <div class="site-logo">
 <div class="basel-logo-wrap"> 
 <a href="Shop.php?username=<?php echo $_GET['username'];?>&auth=<?php echo $_GET['auth'];?>" class="basel-logo basel-main-logo" rel="home"> 
-<img  src="https://res.cloudinary.com/badboylu/image/upload/c_scale,h_350/v1595757623/website_logo_head_aswjjx.jpg" alt="Basel" /> 
+<img  src="https://res.cloudinary.com/ddjrcafre/image/upload/v1620635722/spar-logo_tk4nx8.png" alt="Basel" /> 
 </a></div></div>
 <div class="right-column">
 <div class="search-button basel-search-full-screen"> <a href="#"> <i class="fa fa-search"></i> </a><div class="basel-search-wrapper"><div class="basel-search-inner"> <span class="basel-close-search">close</span><form role="search" method="get" id="searchform" class="searchform  basel-ajax-search" action="https://demo.xtemos.com/basel/"  data-thumbnail="1" data-price="1" data-count="5" data-post_type="product" data-symbols_count="3" data-sku="0"><div> <label class="screen-reader-text">Search for:</label> <input type="text" class="search-field" placeholder="Search for products" value="" name="s" id="s" /> <input type="hidden" name="post_type" id="post_type" value="product"> <button type="submit" id="searchsubmit" value="Search">Search</button></div></form><div class="search-results-wrapper"><div class="basel-scroll"><div class="basel-search-results basel-scroll-content"></div></div></div></div></div></div>
