fd86461514548d1caf8506ae139b04d433faa17b
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-05-14 13:33:37 +0200

    Update HomeMadeQuiches.php

--- HomeMadeQuiches.php
+++ HomeMadeQuiches.php
@@ -168,7 +168,7 @@ Cafe`
 </span>
 </div>
 <span class="title-after_title">
-	by Bean tree cafe`
+	by the Bean tree cafe`
 </span></div><div class="woocommerce"><div class="single-product" data-product-page-preselected-id="0"><div class="single-breadcrumbs-wrapper"><div class="container"> <a href="javascript:baselThemeModule.backHistory()" class="basel-back-btn basel-tooltip"><span>Back</span></a><nav class="woocommerce-breadcrumb">
 <a href="Shop.php?username=<?php echo $_GET['username'];?>&auth=<?php echo $_GET['auth'];?>">
 Cafe`
