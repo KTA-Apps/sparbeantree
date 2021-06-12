4ee21c8603a4b5e55001fa2d1863470f88aa1ce0
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-06-12 23:20:33 +0200

    Update Bill.php

--- Bill.php
+++ Bill.php
@@ -533,8 +533,9 @@ class="single-product-page single-product-content product-design-default product
                 <span class="totalDelivery">R15</span>
 		<br>
                 <span class="totalCart"><strong>Items</strong></span>
-                <div class="Rsymbol"><span class="totalOrderR">R</span></div>
                 <div class="Totalnums"><span class="totalOrder"></span></div>
+                <div class="Rsymbol"><span class="totalOrderR">R</span></div>
+                
                         </div>
                         <div class='plugfooterEnd'></div>
                 <div class="totalsRow">
