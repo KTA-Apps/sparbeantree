15b97a7c4ab360c9ffbcae63e90b6017e1f327d1
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-06-13 13:31:53 +0200

    Update delivery.php

--- delivery.php
+++ delivery.php
@@ -224,15 +224,16 @@ Delivery details
 <select class="JHBRandburg" name="suburbs" id="suburbsJHBRandburg" form="plug" required>
   <option value="" disabled selected>Select Suburb</option>
   <option value="BeverlyGardens"> Beverly Gardens </option>
-  <option value="Blackheath"> Blackheath </option>
   <option value="Bordeaux"> Bordeaux </option>
+  <option value="Bryanbrink"> Bryanbrink </option>
+  <option value="Cramersview"> Cramersview </option>
   <option value="DanielBrinkPark"> Daniel Brink Park </option>
   <option value="Ferndale"> Ferndale </option>
   <option value="KensingtonB"> Kensington B </option>
+  <option value="Lymepark"> Lyme park </option>
+  <option value="Millhill"> Mill hill </option>
+  <option value="Osummit"> Osummit </option>
   <option value="RandburgCentral"> Randburg Central </option>
-  <option value="RobinAcres"> Robin Acres </option>
-  <option value="RobinHills"> Robin Hills </option>
-  <option value="Robindale"> Robindale </option>
   <option value="VandiaGrove"> Vandia Grove </option>
   <option value="Null" style="display:none" >Null</option>
 </select>
