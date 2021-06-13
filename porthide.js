ae5326e6e92c38af8bcb3ba98cc2dc2312614c6b
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-06-13 17:30:14 +0200

    Create porthide.js

---
+++ porthide.js
@@ -0,0 +1,9 @@
+setInterval (function hideJHB(){
+    var City = document.getElementById('city').value;
+         if (City=='Tshwane/Pretoria'){
+         document.getElementById("hidePTAtown").style.display = "block";
+                 document.getElementById("townsJHB").removeAttribute("required");
+     }else{
+         document.getElementById("hidePTAtown").style.display = "none";
+     }
+ }, 1000);
